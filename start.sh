372cdd1b9a7012c084345166ff0cb185952b75ec
Yash Suhagiya <yashsuhagiya401@gmail.com>
2024-02-13 12:07:18 +0100

    remove sudo

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 # Install dependencies
-sudo yum update -y
-sudo yum install -y curl gnupg
+yum update -y
+yum install -y curl gnupg
 
 # Install .NET Core runtime
-sudo rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
-sudo yum install -y dotnet-runtime-7.0
+rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
+yum install -y dotnet-runtime-7.0
 
 # Build the ASP.NET Core project
 dotnet build
@@ -13,11 +13,11 @@ dotnet build
 dotnet publish -c Release -o ~/publish
 
 # Set up a reverse proxy using Nginx
-sudo yum install -y nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
-sudo rm -f /etc/nginx/nginx.conf
-sudo bash -c 'cat << EOF > /etc/nginx/nginx.conf
+yum install -y nginx
+systemctl start nginx
+systemctl enable nginx
+rm -f /etc/nginx/nginx.conf
+bash -c 'cat << EOF > /etc/nginx/nginx.conf
 server {
     listen 80;
     server_name programming-quotes-api-gamma.vercel.app;
